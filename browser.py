b3e433d8ed6605bfe613eb0212bdfe17cd0c4408
Farouq Umar <ufakz99@gmail.com>
2022-06-23 23:17:44 +0100

    init: telnet clone

---
+++ browser.py
@@ -0,0 +1,69 @@
+import socket
+import ssl
+
+
+def request(url):
+    scheme, url = url.split("://", 1)
+    assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
+
+    host, path = url.split('/', 1)
+    path = "/" + path
+
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP
+    )
+
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
+
+    port = 80 if scheme == "http" else 443
+
+    s.connect((host, port))
+
+    s.send("GET {} HTTP/1.0\r\n".format(path).encode("utf8") +
+           "Host: {}\r\n\r\n".format(host).encode("utf8"))
+
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n":
+            break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+
+    assert "transfer-encoding" not in headers
+    assert "content-encoding" not in headers
+
+    body = response.read()
+    s.close()
+
+    return headers, body
+
+
+def show(body):
+    in_angle = False
+    for c in body:
+        if c == "<":
+            in_angle = True
+        elif c == ">":
+            in_angle = False
+        elif not in_angle:
+            print(c, end="")
+
+
+def load(url):
+    headers, body = request(url)
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+    load(sys.argv[1])
